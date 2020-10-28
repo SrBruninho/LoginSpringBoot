b1a2780e7d0c8a0da40f915b2079c6b6d4f77522
fakebrunosilva <bos.bruno.oliveira55@gmail.com>
2020-10-27 22:44:34 -0300

    SecurityConfig e MainApplication

--- src/main/java/srbruninho/MainApplication.java
+++ src/main/java/srbruninho/MainApplication.java
@@ -1,4 +1,19 @@
 package srbruninho;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class MainApplication {
+
+    @GetMapping("/home")
+    public String paginaInicial(){
+        return "Página Inicial";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
